58143cb9502820f5274729f14973adc00407c69e
Alec <Alec@ALEC-DESKTOP>
2015-10-24 14:26:25 -0700

    Added createAccount method to SQLiteDB. Adds id to general username table.

--- CSE360/src/SQLiteDB.java
+++ CSE360/src/SQLiteDB.java
@@ -1,19 +1,37 @@
+import java.io.File;
+import java.sql.*;
+
+
+
 /** An implementation of the Database class, 
  * SQLiteDB contains all the methods necessary to push and pull
  *  data from the locally stored database.
  * @author Alec Oldenburg
- * 
+ * @version 1.0
  */
 public class SQLiteDB implements Database {
-
-	/** Adds an account to the 
+	
+	/** Adds an account to the user login database
+	 *  as well as their appropriate table (patient, doctor, staff, etc.).
 	 * 
-	 * 
-	 * 
+	 *  @param a The account to be added.
 	 */
 	public void createAccount(Account a) {
 		
-
+		try {
+			if(a == null) throw new IllegalArgumentException("Account passed was null");
+			File file = new File("data/EmployeeData.sqlite");
+			file.getAbsolutePath();
+			Class.forName("org.sqlite.JDBC");
+			Connection connector=DriverManager.getConnection("jdbc:sqlite:" + file.getAbsolutePath());
+			String query = "INSERT INTO UserTable (username, password, type) VALUES (\'" + a.getUserName() + "\', "
+					+ "\'" + a.getPassword() + "\', \'" + a.getAccountType().toString() + "\')";
+			Statement st = connector.createStatement();
+			st.executeUpdate(query);
+		} catch (SQLException | ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	public Account accessAccount(String username, String password) {
