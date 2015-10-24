8d8f063cba85de6976602848cac91b1d1fe120c1
Jacob <jacob8995@gmail.com>
2015-10-24 14:38:49 -0700

    Gui for the Nurse

--- CSE360/src/NurseMenuWindow.java
+++ CSE360/src/NurseMenuWindow.java
@@ -10,7 +10,7 @@ public class NurseMenuWindow extends JFrame{
 	private JPanel contentPane;
 
 	/**
-	 * Launch the application.
+	 * Launch the application
 	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
