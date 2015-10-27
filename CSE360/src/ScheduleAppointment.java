c0c5ed9fa66bff08bc8ee3a6ffaf35170d8510de
Zachary Brotman <zbrotman@asu.edu>
2015-10-27 10:56:29 -0700

    Changes to Format and changed JTextField to JTextArea
    I used a JTable instead of the TextArea. I wanted to make the table
    dynamic so that it added a row whenever an appointment was added but I
    don't know where the table will be pulling the information from.

--- CSE360/src/ScheduleAppointment.java
+++ CSE360/src/ScheduleAppointment.java
@@ -10,11 +10,18 @@ import javax.swing.JCheckBox;
 import javax.swing.JDialog;
 import javax.swing.JTextField;
 import javax.swing.JButton;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JFormattedTextField;
+import javax.swing.JTextArea;
+import java.awt.Font;
+import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
 
 public class ScheduleAppointment extends JDialog {
 
 	private JPanel contentPane;
-	private JTextField textFieldReason;
 
 	/**
 	 * Launch the application.
@@ -40,112 +47,126 @@ public class ScheduleAppointment extends JDialog {
 		setBounds(100, 100, 493, 561);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null); 
+		setContentPane(contentPane);//<<<<<<< HEAD//=======
+		contentPane.setLayout(null); //>>>>>>> branch 'master' of https://github.com/aoldenburg/F15-CSE360-Project.git
 		
 		JLabel lblHaveYouSeen = new JLabel("Have you seen this doctor before?*");
-		lblHaveYouSeen.setBounds(10, 11, 172, 14);
-		contentPane.add(lblHaveYouSeen);
+		lblHaveYouSeen.setBounds(10, 11, 225, 14);
 		
 		JRadioButton rdbtnYes = new JRadioButton("Yes");
 		rdbtnYes.setBounds(10, 32, 64, 23);
-		contentPane.add(rdbtnYes);
 		
 		JRadioButton rdbtnNo = new JRadioButton("No");
-		rdbtnNo.setBounds(110, 32, 109, 23);
-		contentPane.add(rdbtnNo);
+		rdbtnNo.setBounds(75, 32, 95, 23);
 		
 		JLabel lblDoYouPrefer = new JLabel("Do you Prefer a Male or Female Physician?*");
-		lblDoYouPrefer.setBounds(10, 86, 209, 14);
-		contentPane.add(lblDoYouPrefer);
+		lblDoYouPrefer.setBounds(10, 86, 250, 14);
 		
 		JRadioButton rdbtnMale = new JRadioButton("Male");
 		rdbtnMale.setBounds(6, 107, 70, 23);
-		contentPane.add(rdbtnMale);
 		
 		JRadioButton rdbtnFemale = new JRadioButton("Female");
-		rdbtnFemale.setBounds(110, 107, 109, 23);
-		contentPane.add(rdbtnFemale);
+		rdbtnFemale.setBounds(75, 107, 109, 23);
 		
-		JLabel lblWhatDaysOf = new JLabel("What Days of the week are you available?*");
+		JLabel lblWhatDaysOf = new JLabel("What days of the week are you available?*");
 		lblWhatDaysOf.setBounds(10, 162, 247, 14);
-		contentPane.add(lblWhatDaysOf);
 		
 		JCheckBox chckbxMonday = new JCheckBox("Monday");
-		chckbxMonday.setBounds(6, 184, 64, 23);
-		contentPane.add(chckbxMonday);
+		chckbxMonday.setBounds(10, 184, 85, 23);
 		
 		JCheckBox chckbxTuesday = new JCheckBox("Tuesday");
-		chckbxTuesday.setBounds(68, 184, 70, 23);
-		contentPane.add(chckbxTuesday);
+		chckbxTuesday.setBounds(100, 184, 85, 23);
 		
 		JCheckBox chckbxWednesday = new JCheckBox("Wednesday");
-		chckbxWednesday.setBounds(140, 184, 83, 23);
-		contentPane.add(chckbxWednesday);
+		chckbxWednesday.setBounds(195, 184, 100, 23);
 		
-		JCheckBox chckbxThurday = new JCheckBox("Thurday");
-		chckbxThurday.setBounds(224, 184, 83, 23);
-		contentPane.add(chckbxThurday);
+		JCheckBox chckbxThurday = new JCheckBox("Thursday");
+		chckbxThurday.setBounds(305, 184, 85, 23);
 		
 		JCheckBox chckbxFriday = new JCheckBox("Friday");
-		chckbxFriday.setBounds(6, 210, 64, 23);
-		contentPane.add(chckbxFriday);
+		chckbxFriday.setBounds(10, 210, 85, 23);
 		
 		JCheckBox chckbxSaturday = new JCheckBox("Saturday");
-		chckbxSaturday.setBounds(68, 210, 70, 23);
-		contentPane.add(chckbxSaturday);
+		chckbxSaturday.setBounds(100, 210, 85, 23);
 		
 		JCheckBox chckbxSunday = new JCheckBox("Sunday");
-		chckbxSunday.setBounds(139, 210, 97, 23);
-		contentPane.add(chckbxSunday);
+		chckbxSunday.setBounds(195, 210, 85, 23);
 		
 		JLabel lblWhatTimesOf = new JLabel("What times of the day are you available?*");
-		lblWhatTimesOf.setBounds(10, 266, 209, 14);
-		contentPane.add(lblWhatTimesOf);
+		lblWhatTimesOf.setBounds(10, 266, 240, 14);
 		
 		JLabel lblReasonForAppointment = new JLabel("Reason for Appointment:*");
 		lblReasonForAppointment.setBounds(10, 355, 213, 14);
-		contentPane.add(lblReasonForAppointment);
-		
-		textFieldReason = new JTextField();
-		textFieldReason.setBounds(10, 380, 457, 97);
-		contentPane.add(textFieldReason);
-		textFieldReason.setColumns(10);
 		
 		JButton btnSubmit = new JButton("Submit");
 		btnSubmit.setBounds(378, 488, 89, 23);
-		contentPane.add(btnSubmit);
 		
 		JCheckBox chckbxAm = new JCheckBox("8 am");
-		chckbxAm.setBounds(10, 287, 49, 23);
-		contentPane.add(chckbxAm);
+		chckbxAm.setBounds(10, 287, 65, 23);
 		
-		JCheckBox chckbxAm_1 = new JCheckBox("9 am");
+		JCheckBox chckbxAm_1 = new JCheckBox("9 am");//<<<<<<< HEAD
+		chckbxAm_1.setBounds(78, 287, 65, 23);
+//=======
 		chckbxAm_1.setBounds(78, 287, 56, 23);
 		contentPane.add(chckbxAm_1);
+//>>>>>>> branch 'master' of https://github.com/aoldenburg/F15-CSE360-Project.git
 		
 		JCheckBox chckbxAm_2 = new JCheckBox("10 am");
-		chckbxAm_2.setBounds(153, 287, 66, 23);
-		contentPane.add(chckbxAm_2);
+		chckbxAm_2.setBounds(153, 287, 65, 23);
 		
 		JCheckBox chckbxAm_3 = new JCheckBox("11 am");
-		chckbxAm_3.setBounds(224, 287, 66, 23);
-		contentPane.add(chckbxAm_3);
+		chckbxAm_3.setBounds(224, 287, 65, 23);
 		
 		JCheckBox chckbxPm = new JCheckBox("12 pm");
-		chckbxPm.setBounds(10, 313, 66, 23);
-		contentPane.add(chckbxPm);
+		chckbxPm.setBounds(10, 313, 65, 23);
 		
 		JCheckBox chckbxPm_1 = new JCheckBox("1 pm");
-		chckbxPm_1.setBounds(78, 313, 66, 23);
-		contentPane.add(chckbxPm_1);
+		chckbxPm_1.setBounds(78, 313, 65, 23);
 		
 		JCheckBox chckbxPm_2 = new JCheckBox("2 pm");
-		chckbxPm_2.setBounds(153, 313, 66, 23);
-		contentPane.add(chckbxPm_2);
+		chckbxPm_2.setBounds(153, 313, 65, 23);
 		
 		JCheckBox chckbxPm_3 = new JCheckBox("3 pm");
-		chckbxPm_3.setBounds(224, 313, 66, 23);
+		chckbxPm_3.setBounds(224, 313, 65, 23);
+		contentPane.setLayout(null);
+		contentPane.add(lblHaveYouSeen);
+		contentPane.add(rdbtnYes);
+		contentPane.add(rdbtnNo);
+		contentPane.add(lblDoYouPrefer);
+		contentPane.add(rdbtnMale);
+		contentPane.add(rdbtnFemale);
+		contentPane.add(lblWhatDaysOf);
+		contentPane.add(chckbxMonday);
+		contentPane.add(chckbxTuesday);
+		contentPane.add(chckbxWednesday);
+		contentPane.add(chckbxThurday);
+		contentPane.add(chckbxFriday);
+		contentPane.add(chckbxSaturday);
+		contentPane.add(chckbxSunday);
+		contentPane.add(lblWhatTimesOf);
+		contentPane.add(lblReasonForAppointment);
+		contentPane.add(btnSubmit);
+		contentPane.add(chckbxAm);
+		contentPane.add(chckbxAm_1);
+		contentPane.add(chckbxAm_2);
+		contentPane.add(chckbxAm_3);
+		contentPane.add(chckbxPm);
+		contentPane.add(chckbxPm_1);
+		contentPane.add(chckbxPm_2);
 		contentPane.add(chckbxPm_3);
+		
+		JScrollPane scrollPane = new JScrollPane();// added a Scroll Pane
+		scrollPane.setBounds(10, 380, 457, 100);
+		contentPane.add(scrollPane);
+		
+		JTextArea textArea_1 = new JTextArea(); //changed TextField to TextArea
+		textArea_1.setLineWrap(true);
+		scrollPane.setViewportView(textArea_1);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setFont(new Font("Arial", Font.PLAIN, 12));
+		textArea.setLineWrap(true);
+		textArea.setBounds(10, 380, 440, 95);
+		
 	}
 }
