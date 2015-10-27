c6dc7d5b036d954b6f8ace64510c0e74b6a2ff3f
Zachary Brotman <zbrotman@asu.edu>
2015-10-27 11:11:21 -0700

    Changed TextArea to JTable
    I used a JTable instead of the TextArea. I wanted to make the table
    dynamic so that it added a row whenever an appointment was added but I
    don't know where the table will be pulling the information from.

--- CSE360/src/AppointmentList.java
+++ CSE360/src/AppointmentList.java
@@ -9,6 +9,9 @@ import javax.swing.JLabel;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JList;
+import javax.swing.JScrollPane;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.ListSelectionModel;
 
 public class AppointmentList extends JDialog {
 
@@ -42,28 +45,63 @@ public class AppointmentList extends JDialog {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		table = new JTable();
-		table.setBounds(10, 63, 418, 352);
-		contentPane.add(table);
-		
-		JLabel lblDoctorsName = new JLabel("Doctors Name");
-		lblDoctorsName.setBounds(10, 39, 67, 14);
-		contentPane.add(lblDoctorsName);
-		
-		JLabel lblLocation = new JLabel("Location");
-		lblLocation.setBounds(122, 38, 46, 14);
-		contentPane.add(lblLocation);
-		
-		JLabel lblDay = new JLabel("Day");
-		lblDay.setBounds(217, 39, 46, 14);
-		contentPane.add(lblDay);
-		
-		JLabel lblTime = new JLabel("Time");
-		lblTime.setBounds(297, 38, 46, 14);
-		contentPane.add(lblTime);
-		
 		JButton btnSelect = new JButton("Select");
 		btnSelect.setBounds(339, 426, 89, 23);
 		contentPane.add(btnSelect);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 11, 418, 395);
+		contentPane.add(scrollPane);
+		
+		table = new JTable();
+		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+			},
+			new String[] {
+				"Doctor's Name", "Time", "Day", "Location"
+			}
+		) {
+			boolean[] columnEditables = new boolean[] {
+				false, false, false, false
+			};
+			public boolean isCellEditable(int row, int column) {
+				return columnEditables[column];
+			}
+		});
+		table.getColumnModel().getColumn(0).setResizable(false);
+		table.getColumnModel().getColumn(0).setPreferredWidth(157);
+		table.getColumnModel().getColumn(1).setResizable(false);
+		table.getColumnModel().getColumn(1).setPreferredWidth(100);
+		table.getColumnModel().getColumn(2).setResizable(false);
+		table.getColumnModel().getColumn(2).setPreferredWidth(100);
+		table.getColumnModel().getColumn(3).setResizable(false);
+		table.getColumnModel().getColumn(3).setPreferredWidth(114);
+		scrollPane.setViewportView(table);
+		table.setColumnSelectionAllowed(true);
+		table.setFillsViewportHeight(true);
+		table.setCellSelectionEnabled(true);
 	}
 }
