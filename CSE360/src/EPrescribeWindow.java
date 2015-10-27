83c556191da6c5f5e8cc57786e7264a9c1b50313
Jacob <jacob8995@gmail.com>
2015-10-27 11:29:48 -0700

    EPrescribe Gui created

---
+++ CSE360/src/EPrescribeWindow.java
@@ -0,0 +1,77 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+
+public class EPrescribeWindow extends JFrame{
+	
+	private JPanel contentPane;
+	
+	public static void main(String[] args){
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					EPrescribeWindow frame = new EPrescribeWindow();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		
+	}
+	public EPrescribeWindow(){
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 454, 400);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		setTitle("E-Prescription");
+		
+		JLabel lblMedicationName = new JLabel("Medication Name:");
+		lblMedicationName.setBounds(20, 50, 200, 50);
+		lblMedicationName.setFont(new Font("Serif", Font.PLAIN, 24));
+		contentPane.add(lblMedicationName);
+		
+		JLabel lblAmount = new JLabel("Amount Prescribed:");
+		lblAmount.setBounds(10, 150, 200, 50);
+		lblAmount.setFont(new Font("Serif", Font.PLAIN, 24));
+		contentPane.add(lblAmount);
+		
+		JTextField tfMedication = new JTextField();
+		tfMedication.setBounds(220, 50, 200, 50);
+		tfMedication.setFont(new Font("Serif", Font.PLAIN, 24));
+		contentPane.add(tfMedication);
+		
+		JTextField tfAmount = new JTextField();
+		tfAmount.setBounds(220, 150, 200, 50);
+		tfAmount.setFont(new Font("Serif", Font.PLAIN, 24));
+		contentPane.add(tfAmount);
+		
+		JButton btnPrescribe = new JButton("Prescribe");
+		btnPrescribe.setBounds(50, 250, 150, 50);
+		btnPrescribe.setFont(new Font("Serif", Font.PLAIN, 24));
+		btnPrescribe.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		contentPane.add(btnPrescribe);
+		
+		JButton btnCancel = new JButton("Cancel");
+		btnCancel.setBounds(250, 250, 150, 50);
+		btnCancel.setFont(new Font("Serif", Font.PLAIN, 24));
+		btnPrescribe.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		contentPane.add(btnCancel);
+		
+	}
+	
+}
+
